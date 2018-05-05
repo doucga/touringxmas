1412a7828217fcc2e70b5114da148dc17050572b
doucga <doucga@users.noreply.github.com>
2018-05-04 18:24:43 -0700

    Background

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 <meta http-equiv="content-type" content="text/html; charset=utf-8" >
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
-<body style="background:url(\"{{url('/images/xmasbackground.jpg')}}\"); background-repeat:no-repeat; background-size:100% 100%">
+<body style="background:url("{{url('/images/xmasbackground.jpg')}}"); background-repeat:no-repeat; background-size:100% 100%">
 
 </body>
 </html>
