b7554a8a18f0da6e50f02282ebff266408a2aa30
doucga <doucga@users.noreply.github.com>
2018-05-04 18:05:57 -0700

    Background

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 <meta http-equiv="content-type" content="text/html; charset=utf-8" >
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
-<body background="{{url('/images/Christmas_tree_03.jpg')}}">
+<body background="{{url('/images/xmasbackground.jpg')}}">
 
 </body>
 </html>
