3055f7fb3f1a3209cb72df2d3f04c2af399318ed
doucga <doucga@users.noreply.github.com>
2018-05-08 17:01:06 -0700

    Misc Commit

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Coming Soon</title>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" >
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>{{config('app.name', 'TouringChrismas.com')}}</title>
 <style>
 body {
   background: url({{url('/images/xmasbackground.jpg')}}) no-repeat;
@@ -18,18 +18,13 @@ body {
     left: 50%;
     top: 50%;
     }
-
-@font-face {
-    font-family: WhiteXmas;
-    src: url('{{ public_path('public/fonts/whitexmas.ttf') }}');
-}
 </style>
 
 
 </head>
 <body>
     <div class="center">
-      <font size="3" face="WhiteXmas" color="white"> Something cool coming soon! </font>
+      <font size="4" color="white"> This is about page! </font>
     </div>
 </body>
 </html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" >
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="{{ asset('css/styles.css') }}" rel="stylesheet" type="text/css" >
+<title>{{config('app.name', 'TouringChrismas.com')}}</title>
+<style>
+    * { margin: 0; padding: 0; }
+    
+    html { 
+        background: url({{url('/images/xmasbackground-small.jpg')}}) no-repeat center center fixed; 
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+    }
+
+</style>
+</head>
+
+<body>
+    <div>            
+        <font class="center" face="mountains_of_christmasregular" color=white><h1> Something cool coming soon! </h1></font>
+    </div>
+</body>
+</html>
